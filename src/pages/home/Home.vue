c6b1d69a9301a40cb56f76645d95e3d7a56f1b97
haojie <1356903710@qq.com>
2018-04-22 13:58:32 +0800

    index finish

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,11 +1,11 @@
 <template>
-	<div>
-		<home-header></home-header>
-		<home-swiper></home-swiper>
-		<home-icons></home-icons>
-		<home-recommend></home-recommend>
-		<home-weekend></home-weekend>
-	</div>
+  <div>
+    <home-header :city="city"></home-header>
+    <home-swiper :list="swiperList"></home-swiper>
+    <home-icons :list="iconsList"></home-icons>
+    <home-recommend :list="recommendList"></home-recommend>
+    <home-weekend :list="weekendList"></home-weekend>
+  </div>
 </template>
 
 <script>
@@ -14,15 +14,45 @@ import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
+import axios from 'axios'
 export default {
-	name: 'Home',
-	components: {
-		HomeHeader,
-		HomeSwiper,
-		HomeIcons,
-		HomeRecommend,
-		HomeWeekend
-	}
+  name: 'Home',
+  components: {
+    HomeHeader,
+    HomeSwiper,
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
+  },
+  data () {
+    return {
+      city: '',
+      swiperList: [],
+      iconsList: [],
+      recommendList: [],
+      WeekendList: []
+    }
+  },
+  methods: {
+    getHomeInfo () {
+      axios.get('/api/index.json')
+        .then(this.getHomeInfoSucc)
+    },
+    getHomeInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.city = data.city
+        this.swiperList = data.swiperList
+        this.iconsList = data.iconsList
+        this.recommendList = data.recommendList
+        this.weekendList = data.weekendList
+      }
+    }
+  },
+  mounted () {
+    this.getHomeInfo()
+  }
 }
 </script>
 
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,44 +1,45 @@
 <template>
-	<div class="wrapper">
-		<swiper :options="swiperOption">
-	    <swiper-slide v-for="item of swiperList" :key="item.id">
-	    	<img class="swiper-img" :src="item.imgUrl" />
-	    </swiper-slide>
-	    <div class="swiper-pagination"  slot="pagination"></div>
-	  </swiper>
+  <div class="wrapper">
+    <swiper :options="swiperOption" v-if="showSwiper">
+      <swiper-slide v-for="item of list" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 
 <script>
 export default {
-	name: 'HomeSwiper',
-	data () {
-		return {
-			swiperOption: {
-				pagination: '.swiper-pagination',
-				loop: true
-			},
-			swiperList: [{
-				id: '0001',
-				imgUrl: 'http://img1.qunarzz.com/piao/fusion/1712/56/7e62e24499ad4702.jpg_890x330_dd59942e.jpg'
-			}, {
-				id: '0002',
-				imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/9e/b9f4b2498b291e02.jpg_890x330_36ccc796.jpg'
-			}]
-		}
-	}
+  name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      }
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-	.wrapper >>> .swiper-pagination-bullet-active
-	  background: #fff !important
-	.wrapper
-	  overflow: hidden
-	  width: 100%
-	  height: 0
-	  padding-bottom: 37.08%
-	  background: #eee
-	  .swiper-img
-	    width: 100%
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff !important
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 37.08%
+    background: #eee
+    .swiper-img
+      width: 100%
 </style>
